d2928fade209a7f6ce34d66e2e554cc43e75def8
Bastien <bastien-stachs@hotmail.fr>
2026-01-14 10:52:43 +0100

    Act. 6. Les gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // modifier / supprimer un produit
+        Gate::define('manage-product', function (User $user, Product $product) {
+            return $user->id === $product->user_id;
+        });
+
+        // voir un produit
+        Gate::define('view-product', function (User $user, Product $product) {
+            return $user->id === $product->user_id || $product->is_public;
+        });
     }
 
     /**
